312fc1e1dbb52400bf912a5994dfb06a128ca960
Vincent Romine <vinjrom@live.com>
2019-12-21 14:36:33 -0600

    restructure files

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to CryptoCurrent!
         </p>
         <a
           className="App-link"
