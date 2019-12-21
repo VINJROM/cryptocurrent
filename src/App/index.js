ea57cea0c8b22259a9b9768418d98ee82f633cdc
Vincent Romine <vinjrom@live.com>
2019-12-21 14:44:46 -0600

    welcome message

--- src/App/index.js
+++ src/App/index.js
@@ -1,26 +1,11 @@
-import React , {component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Welcome from "./WelcomeMessage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to CryptoCurrent!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <Welcome />;
+  }
 }
 
 export default App;
