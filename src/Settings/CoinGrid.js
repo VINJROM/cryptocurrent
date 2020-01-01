6ef2e1378b61276630176ae40464f8dd769a0850
Vincent Romine <vinjrom@live.com>
2019-12-31 19:25:44 -0600

    create CoinGrid

---
+++ src/Settings/CoinGrid.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+import {AppContext} from '../App/AppProvider';
+
+export const CoinGridStyled = styled.div`
+    display: grid;
+`
