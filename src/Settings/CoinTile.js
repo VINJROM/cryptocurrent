12106ba7232b17aecd490d5fd088f137f7c16e3e
Vincent Romine <vinjrom@live.com>
2020-01-02 23:41:05 -0600

    add CoinTile

---
+++ src/Settings/CoinTile.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { AppContext } from "../App/AppProvider";
+import { SelectableTile } from "../Shared/Tile";
+
+export default function({coinKey}){
+
+    
+    return <AppContext.Consumer>
+        {({coinList}) =>{
+            let coin = coinList[coinKey];
+            
+            const TileClass = SelectableTile;
+            return <TileClass>
+                {coinKey}
+            </TileClass>
+        }}
+    </AppContext.Consumer>
+}
