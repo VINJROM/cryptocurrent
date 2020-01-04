301e9eb20396e72cb1a7ff544cc78d6d7ae68c1c
Vincent Romine <vinjrom@live.com>
2020-01-03 20:00:39 -0600

    add margin top 40px

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -7,6 +7,7 @@ export const CoinGridStyled = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-gap: 15px;
+  margin-top: 40px;
 `;
 
 function getCoinsToDisplay(coinList) {
