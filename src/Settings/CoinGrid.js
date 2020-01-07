cbdec5534540bb06d0dbd5cdbf3e29dafd7ce6e7
Vincent Romine <vinjrom@live.com>
2020-01-07 00:03:36 -0600

    import topsection to coinTile

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -21,7 +21,7 @@ export default function({ topSection }) {
       {({ coinList }) => (
         <CoinGridStyled>
           {getCoinsToDisplay(coinList, topSection).map(coinKey => (
-            <CoinTile coinKey={coinKey} />
+            <CoinTile topSection={topSection} coinKey={coinKey} />
           ))}
         </CoinGridStyled>
       )}
--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -2,23 +2,24 @@ import React from "react";
 import { AppContext } from "../App/AppProvider";
 import { SelectableTile, DisabledTile, DeletableTile } from "../Shared/Tile";
 import CoinHeaderGrid from "./CoinHeaderGrid";
-import CoinImage from "../Shared/CoinImage"
-
+import CoinImage from "../Shared/CoinImage";
 
 // pulls CoinName, Symbol, and image from CC
-export default function({ coinKey }) {
+export default function({ coinKey, topSection }) {
   return (
     <AppContext.Consumer>
       {({ coinList }) => {
         let coin = coinList[coinKey];
 
-        const TileClass = SelectableTile;
+        let TileClass = SelectableTile;
+        if (topSection) {
+          TileClass = DeletableTile;
+        }
 
-        
         return (
           <TileClass>
             <CoinHeaderGrid name={coin.CoinName} symbol={coin.Symbol} />
-            <CoinImage coin={coin}/>
+            <CoinImage coin={coin} />
           </TileClass>
         );
       }}
