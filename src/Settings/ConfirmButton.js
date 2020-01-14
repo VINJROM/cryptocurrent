0e8a7769927fb8a1cf049161286a4ac68629c3ba
Vincent Romine <vinjrom@live.com>
2020-01-13 22:48:46 -0600

    import greenBoxShadow

--- src/Settings/ConfirmButton.js
+++ src/Settings/ConfirmButton.js
@@ -4,12 +4,13 @@ import { AppContext } from "../App/AppProvider";
 import { fontSize1, greenBoxShadow, color3 } from "../Shared/Styles";
 
 const ConfirmButtonStyled = styled.div`
-  margin: 20px;
-  color: ${color3} ${fontSize1};
-  padding: 5px;
-  cursor: pointer;
-  &:hover {
-    ${greenBoxShadow}
+margin: 20px;
+color: ${color3}
+${fontSize1} 
+padding: 5px;
+cursor: pointer; 
+&:hover {
+  ${greenBoxShadow} 
   }
 `;
 
--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -1,6 +1,26 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import {backgroundColor2, fontSize2} from "../Shared/Styles"
+
+const SearchGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const SearchInput = styled.input`
+  ${backgroundColor2}
+  ${fontSize2}
+  border: 1px solid; 
+  height: 25px; 
+  color: #1163c9;
+  place-self: center left; 
+`;
 
 export default () => {
-    return <div>Search</div>
-}
+  return (
+    <SearchGrid>
+      <h2>Search all coins</h2>
+      <SearchInput />
+    </SearchGrid>
+  );
+};
