88cb978a37877ae674e8f8d9ee115bff822e06a8
Vincent Romine <vinjrom@live.com>
2020-01-17 20:35:25 -0600

    debounce input

--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import {backgroundColor2, fontSize2} from "../Shared/Styles"
+import { backgroundColor2, fontSize2 } from "../Shared/Styles";
+import { AppContext } from "../App/AppProvider";
+import _ from "lodash";
 
 const SearchGrid = styled.div`
   display: grid;
@@ -10,18 +12,33 @@ const SearchGrid = styled.div`
 const SearchInput = styled.input`
   ${backgroundColor2}
   ${fontSize2}
-  border: 1px solid; 
-  height: 25px; 
+  border: 1px solid;
+  height: 25px;
   color: #1163c9;
-  place-self: center left; 
+  place-self: center left;
 `;
+
+const handleFilter = _.debounce((inputValue, coinList, setFilterCoins) => {
+  console.log(inputValue);
+}, 500); // debounce delays invoking function displaying input value
+
+function filterCoins(e, setFilteredCoins, coinList) {
+  let inputValue = e.target.value; // targets input value to be captured
+  handleFilter(inputValue, coinList, setFilteredCoins)
+}
 
 // displays search-confirmation and input-field
 export default () => {
   return (
-    <SearchGrid>
-      <h2>Search all coins</h2>
-      <SearchInput />
-    </SearchGrid>
+    <AppContext.Consumer>
+      {({ setFilteredCoins, coinList }) => (
+        <SearchGrid>
+          <h2>Search all coins</h2>
+          <SearchInput
+            onKeyUp={e => filterCoins(e, setFilteredCoins, coinList)} // "filterCoins" acts as a helper function that passes an event "e" to pull from all coins and set-coins
+          />
+        </SearchGrid>
+      )}
+    </AppContext.Consumer>
   );
 };
