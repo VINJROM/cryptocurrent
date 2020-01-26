45d34a444e082212243cb49074f6a8ec58b11783
Vincent Romine <vinjrom@live.com>
2020-01-25 20:36:55 -0600

    style CoinSpotlight

--- src/Dashboard/CoinSpotlight.js
+++ src/Dashboard/CoinSpotlight.js
@@ -1,15 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 import { Tile } from "../Shared/Tile";
 import { AppContext } from "../App/AppProvider";
 import CoinImage from "../Shared/CoinImage";
 
+const SpotLightName = styled.h2`
+  text-align: center;
+`;
+
+// displays stylized coin symbol and image
 export default () => {
   return (
     <AppContext.Consumer>
       {({ currentFavorite, coinList }) => (
         <Tile>
-          <h2>{coinList[currentFavorite].CoinName}</h2>
-          <CoinImage coin={coinList[currentFavorite]} />
+          <SpotLightName>{coinList[currentFavorite].CoinName}</SpotLightName>
+          <CoinImage spotlight coin={coinList[currentFavorite]} />
         </Tile>
       )}
     </AppContext.Consumer>
